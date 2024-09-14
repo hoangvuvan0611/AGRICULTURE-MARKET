06cb377b2f8929c1a047f6175d5a67658aca0223
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2024-09-14 16:35:59 +0700

    first commit

---
+++ src/test/java/com/agrimarket/AgriMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
