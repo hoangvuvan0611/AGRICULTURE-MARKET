51fb8f1f17eca1a229bfe60ac18d7026529e9d83
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2024-09-15 11:08:04 +0700

    Entity Role(vai tro)- day du cac thuoc tinh can co

--- src/main/java/com/agrimarket/entity/Role.java
+++ src/main/java/com/agrimarket/entity/Role.java
@@ -1,14 +1,40 @@
 package com.agrimarket.entity;
 
-import jakarta.persistence.Entity;
+import com.agrimarket.constant.enums.RoleEnum;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity()
+@Entity(name = "ROLES")
+@Table(name = "ROLES")
 public class Role {
-    private String role;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column( name = "role_name", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleEnum roleName;
+
+    @CreationTimestamp
+    @Column(name = "creation_date")
+    private Timestamp creationDate;
+
+    @UpdateTimestamp
+    @Column(name = "last_update_date")
+    private Timestamp lastUpdateDate;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<Staff> staffSet = new HashSet<>();
 }
