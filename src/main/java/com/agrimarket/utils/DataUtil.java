ba773f888b47d4e7fd924e31220e1dbdff63da55
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2024-09-22 13:04:15 +0700

    cac ham util

--- src/main/java/com/agrimarket/utils/DataUtil.java
+++ src/main/java/com/agrimarket/utils/DataUtil.java
@@ -10,4 +10,46 @@ public class DataUtil {
     public static boolean isNullObject(Object o) {
         return o == null;
     }
+
+    public static Long safeToLong(Object o) {
+        if(isNullObject(o)) {
+            return 0L;
+        }
+        return Long.parseLong(o.toString());
+    }
+
+    public static Double safeToDouble(Object o) {
+        if(isNullObject(o)) {
+            return 0D;
+        }
+        return Double.parseDouble(o.toString());
+    }
+
+    public static Float safeToFloat(Object o) {
+        if(isNullObject(o)) {
+            return 0F;
+        }
+        return Float.parseFloat(o.toString());
+    }
+
+    public static Integer safeToInt(Object o) {
+        if(isNullObject(o)) {
+            return 0;
+        }
+        return Integer.parseInt(o.toString());
+    }
+
+    public static Boolean safeToBoolean(Object o) {
+        if(isNullObject(o)) {
+            return false;
+        }
+        return Boolean.parseBoolean(o.toString());
+    }
+
+    public static String safeToString(Object o) {
+        if(isNullObject(o)) {
+            return "";
+        }
+        return o.toString();
+    }
 }
